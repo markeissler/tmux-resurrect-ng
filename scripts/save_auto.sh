af6454f0acf380b808d4b098665c42eb228db0ee
Mark Eissler <mark@mixtur.com>
2015-01-26 18:05:48 -0800

    Fixes for syntax and style quibbles.

--- scripts/save_auto.sh
+++ scripts/save_auto.sh
@@ -28,38 +28,40 @@ update_pane_trigger() {
 	local trigger_file_path="$(resurrect_trigger_file "$pane_id" "$pane_tty")"
 	local timeinsec=$(date +%s)
 	local frequency=$(save_auto_frequency) # minutes
-	local frequency_sec=$(( $frequency * 60 ))
+	local frequency_sec=$(( frequency * 60 ))
 	local return_status=0
 
 	# if history/buffer files are missing or one of them is old, update trigger
-	local buffer_file_mtime=$( (stat_mtime $buffer_file_path) || echo 0 )
-	local buffer_file_staterr=$?
-	local history_file_mtime=$( (stat_mtime $history_file_path) || echo 0 )
-	local history_file_staterr=$?
+	local buffer_file_mtime=$( (stat_mtime $buffer_file_path) || echo -1 )
+	local buffer_file_staterr=0
+	local history_file_mtime=$( (stat_mtime $history_file_path) || echo -1 )
+	local history_file_staterr=0
+	[[ $buffer_file_mtime -lt 0 ]] && buffer_file_staterr=1
+	[[ $history_file_mtime -lt 0 ]] && history_file_staterr=1
 
 	# Status reflected in tmux status bar is always behind by one check.
 	#
 	if [[ $buffer_file_staterr -ne 0 || $history_file_staterr -ne 0 ]]; then
 		# no history OR buffer file exists
-		return_status=-2
-	elif [[ $(( $timeinsec - $buffer_file_mtime )) -gt $frequency_sec \
-		|| $(( $timeinsec - $history_file_mtime )) -gt $frequency_sec ]]; then
+		return_status=2
+	elif [[ $(( timeinsec - buffer_file_mtime )) -gt $frequency_sec \
+		|| $(( timeinsec - history_file_mtime )) -gt $frequency_sec ]]; then
 		# stale history OR buffer file exists
-		return_status=-1
+		return_status=1
 	fi
 
 	# save updated history/buffers files if no files exist or files are stale
 	if [ $debug -ne 0 ]; then
 		local debug_file_path="/tmp/tmxr_${pane_id}:${pane_tty//\//@}.txt"
-		echo "    	time_now: $timeinsec" > $debug_file_path
-		echo " buffer_mtime: $buffer_file_mtime" > $debug_file_path
-		echo "   buffer_age: $(( $timeinsec - $buffer_file_mtime ))" >> $debug_file_path
-		echo "history_mtime: $history_file_mtime" >> $debug_file_path
-		echo "  history_age: $(( $timeinsec - $history_file_mtime ))" >> $debug_file_path
-		echo " trigger_path: $trigger_file_path" >> $debug_file_path
-		echo "  update_code: $return_status" >> $debug_file_path
+		echo "     time_now: $timeinsec" > "$debug_file_path"
+		echo " buffer_mtime: $buffer_file_mtime" >> "$debug_file_path"
+		echo "   buffer_age: $(( timeinsec - buffer_file_mtime ))" >> "$debug_file_path"
+		echo "history_mtime: $history_file_mtime" >> "$debug_file_path"
+		echo "  history_age: $(( timeinsec - history_file_mtime ))" >> "$debug_file_path"
+		echo " trigger_path: $trigger_file_path" >> "$debug_file_path"
+		echo "  update_code: $return_status" >> "$debug_file_path"
 	fi
-	[[ $return_status -lt 0 ]] && touch ${trigger_file_path}
+	[[ $return_status -gt 0 ]] && touch "${trigger_file_path}"
 
 	return $return_status
 }
@@ -69,7 +71,8 @@ update_pane_triggers() {
 
 	while IFS=$'\t' read line_type session_name window_number window_name window_active window_flags pane_index dir pane_active pane_command full_command; do
 		update_pane_trigger "$session_name:$window_number.$pane_index"
-		[[ $? -lt $return_status ]] && return_status=$?
+		local rslt=$?
+		[[ $rslt -gt $return_status ]] && return_status=$rslt
 	done < <(dump_panes)
 
 	return $return_status
@@ -80,36 +83,37 @@ update_state() {
 	local state_file_path=""
 	local timeinsec=$(date +%s)
 	local frequency=$(save_auto_frequency) # minutes
-	local frequency_sec=$(( $frequency * 60 ))
+	local frequency_sec=$(( frequency * 60 ))
 	local return_status=0
 
 	# find the most-recent layout/state file
 	state_file_path="$(ls -1 $state_file_pattern | sort -r | head -1)"
-	[[ $? -ne 0 ]] && return -129
+	[[ $? -ne 0 ]] && return 255
 
 	# calculate age of layout/state file, save state if old or missing
-	local state_file_mtime=$( (stat_mtime $state_file_path) || echo 0 )
-	local state_file_staterr=$?
+	local state_file_mtime=$( (stat_mtime $state_file_path) || echo -1 )
+	local state_file_staterr=0
+	[[ $state_file_mtime -lt 0 ]] && state_file_staterr=1
 
 	# Status reflected in tmux status bar is always behind by one check.
 	#
-	if [[ $stat_file_staterr -ne 0 ]]; then
+	if [[ $state_file_staterr -ne 0 ]]; then
 		# no state file exists
-		return_status=-2
-	elif [[ $(( $timeinsec - $state_file_mtime )) -gt $frequency_sec ]]; then
+		return_status=2
+	elif [[ $(( timeinsec - state_file_mtime )) -gt $frequency_sec ]]; then
 		# stale state file exists
-		return_status=-1
+		return_status=1
 	fi
 
 	# save updated state if no file exists or file is stale
 	if [ $debug -ne 0 ]; then
 		local debug_file_path="/tmp/tmxr_${pane_id%%:*}.txt"
-		echo "   time_now: $timeinsec" > $debug_file_path
-		echo "state_mtime: $state_file_mtime" >> $debug_file_path
-		echo "  state_age: $(( $timeinsec - $state_file_mtime ))" >> $debug_file_path
-		echo "update_code: $return_status" >> $debug_file_path
+		echo "   time_now: $timeinsec" > "$debug_file_path"
+		echo "state_mtime: $state_file_mtime" >> "$debug_file_path"
+		echo "  state_age: $(( timeinsec - state_file_mtime ))" >> "$debug_file_path"
+		echo "update_code: $return_status" >> "$debug_file_path"
 	fi
-	[[ $return_status -lt 0 ]] && save_all_states
+	[[ $return_status -gt 0 ]] && save_all_states
 
 	return $return_status
 }
